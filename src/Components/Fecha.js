4a4c60409f70d8014a2123eddb6000c50f85be95
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-12-02 21:01:54 -0300

    fix date

--- src/Components/Fecha.js
+++ src/Components/Fecha.js
@@ -29,7 +29,7 @@ const Fecha = () => {
         icono={<BiChurch className="cIcon" />}
         titulo="Ceremonia"
         localidad="Ituzaingó"
-        fecha="07 de Enero del 2022 - 19:30 hrs."
+        fecha="08 de Enero del 2022 - 19:30 hrs."
         direccion="Cnel. Martiniano Chilavert 890"
         gmapsdir="https://www.google.com/maps/place/Iglesia+Cristo+Vive+y+es+Verdad/@-34.6425978,-58.6697704,15z/data=!4m5!3m4!1s0x0:0x5d7edcd75bd49588!8m2!3d-34.6425978!4d-58.6697704"
       />
